41aa02790656f22a6e15ce23637c923e0d56d833
Stephan Raab <stephan.raab@bd.com>
2024-11-13 17:08:58 -0500

    add delete functionality, and button

--- src/components/BaseTodo.vue
+++ src/components/BaseTodo.vue
@@ -15,6 +15,9 @@ export default {
 
             this.todos.push(this.todoInput)
             this.todoInput = ''
+        },
+        deleteTodo(index) {
+            this.todos.splice(index, 1) //the second argument here specifies how many elements to remove
         }
     }
 }
@@ -30,7 +33,7 @@ export default {
         ~~2. do the same thing when hitting enter or the button~~
         ~~3. show todos in list on the screen~~
         4. todos have state uncompleted|completed( completed will be greyed out and crossed through)
-        5. todo can be deleted, removed from the todolist
+        ~~5. todo can be deleted, removed from the todolist~~
         6. you can add a due date to a todo
         7. you can tag a todo with a color
         8. make todos draggable/change order
@@ -40,6 +43,7 @@ export default {
     <label :class="{ 'completed': isCompleted }" v-for="todo, todoIndex in todos" :key="'todo' + todoIndex">
         <input type="checkbox" v-model="isCompleted">
         {{ todo }}
+        <button @click="deleteTodo(todoIndex)" class="deleteBtn">delete</button>
     </label>
 </template>
 
@@ -48,6 +52,7 @@ export default {
 input {
     height: 34px;
     text-align: center;
+    margin-bottom: 1em;
 }
 
 button {
@@ -57,6 +62,7 @@ button {
 label {
     display: flex;
     align-items: center;
+    margin-bottom: .7em;
 }
 
 label>input {
@@ -66,5 +72,9 @@ label>input {
 /* CLASSES */
 .completed {
     text-decoration: line-through;
+}
+
+.deleteBtn {
+    background-color: rgb(129, 46, 46);
 }
 </style>
