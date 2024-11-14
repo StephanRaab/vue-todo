4608aaff02168f581608be87c0a3da8e15719601
Stephan Raab <sarcartist@gmail.com>
2024-11-13 22:03:37 -0500

    add in progress

--- src/components/BaseTodo.vue
+++ src/components/BaseTodo.vue
@@ -67,7 +67,7 @@ export default {
         ~~4. todos have state uncompleted|completed( completed will be greyed out and crossed through)~~
         ~~5. todo can be deleted, removed from the todolist~~
         6. you can add a due date to a todo
-        7. you can tag a todo with a color
+        7. you can tag a todo with a color [IN PROGRESS]
         ~~8. make todos draggable/change order~~
         ~~9. don't allow duplicates~~
         -->
